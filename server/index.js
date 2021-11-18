9f22ee7e61f6a66a4b2f5774a087187b9b53106a
howling1 <45118918+howling1@users.noreply.github.com>
2021-11-18 17:01:43 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const config = require('./config');
 /**
  * Start the server
- * our router is now pointing to /exercises
+ * the router is now pointing to /exercises
  */
 app.listen(config.server.port, function () {
     console.log('Server listening on port 8080!')
